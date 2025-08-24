0af58e11f4fa5ecf396bf8e06815de7ebc2b2229
nldcrafter <nldcrafter1111@gmail.com>
2025-08-24 08:35:14 +0630

    Update app.js

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,5 +1,10 @@
 // Theme Toggle
-const themeToggle = document.querySelector('.theme-toggle');
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.querySelector('.theme-toggle');
+  themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+    themeToggle.innerHTML = document.body.classList.contains('light-mode')
+      ? '<i class="fas fa-sun"></i>'
+      : '<i class="fas fa-moon"></i>';
+  });
 });
